bbe1b9b3f90e1f57ffeae014defbee78dafe9d4e
devcontain <devcontain@users.noreply.github.com>
2026-03-23 20:00:47 +0100

    information_language

--- src/app/shared/language.service.ts
+++ src/app/shared/language.service.ts
@@ -55,12 +55,87 @@ export class LanguageService {
     ]
   };
 
+  private readonly aboutMeDe = {
+    titel: 'ÜBER MICH',
+    paragraphs: [
+      'Geboren wurde ich 1971 in Berlin.',
+      'Nach meiner Ausbildung zur Fachwirtin für Tourismus bin ich für sechs Jahre als Reiseleiterin ins Ausland gegangen. Mittlerweile habe ich meinen Lebensmittelpunkt in Salzwedel gefunden.',
+      'Durch meine Arbeit im Tourismus hatte ich täglich mit vielen Menschen zu tun. Da mich Menschen und ihr Tun schon immer fasziniert haben, habe ich eines Tages beschlossen, ein Fernstudium zu den Grundlagen der Psychologie (SGD) zu absolvieren. Daran angeschlossen haben sich der Studiengang Angst- und Stressbewältigung (SGD) sowie eine Ausbildung zur Psychologischen Beraterin (ALH).',
+      'Zunehmend ist mir jedoch aufgefallen, dass Menschen ihren Blick immer mehr auf die negativen Dinge richten und dadurch immer unzufriedener werden. Aus diesem Grund habe ich mich entschieden, mich der Positiven Psychologie zuzuwenden. Ich absolvierte eine Ausbildung zur Happiness-Trainerin (ALH) und zur Resilienztrainerin. Dabei habe ich gelernt, dass Glück mit relativ einfachen Methoden trainierbar ist, das persönliche Glückspotenzial gesteigert werden kann und ein Perspektivwechsel weg vom Negativen hin zum Positiven einen direkten Einfluss auf unser Wohlbefinden hat.',
+      'Da nur in einem gesunden Körper auch ein gesunder Geist wohnen kann, habe ich meinen Weg mit einer Ausbildung zur Zenbo-Balance-Trainerin abgerundet.',
+      'Um mein Wissen zu erweitern, schlossen sich außerdem Ausbildungen zur Kursleiterin für Beckenbodengymnastik, zur Entspannungstrainerin und zur Brainfit-Trainerin an.',
+      'Außerdem bin ich in den 2. Usui-Reiki-Grad eingeweiht.'
+    ]
+  };
+
+  private readonly resilienceDe = {
+    titel: 'RESILIENZ-TRAINING',
+    paragraphs: [
+      '„Die Kunst ist, einmal mehr aufzustehen, als man umgeworfen wird.“ (Winston Churchill)',
+      'Jeder von uns ist täglich verschiedenen körperlichen und seelischen Belastungen ausgesetzt. Leistungsdruck, Zeitnot und ständig wachsende Anforderungen im Beruf und im Privatleben bestimmen oft genug unseren Alltag. Eine wichtige Rolle bei der Bewältigung dieser täglichen Herausforderungen spielt die mentale Widerstandskraft – die Resilienz.',
+      'Klassischerweise ist das Stehaufmännchen das Sinnbild für Resilienz. Es geht darum, eigene Ressourcen sichtbar und nutzbar zu machen und zu erkennen, dass in jeder Krise und belastenden Situation auch eine Chance stecken kann.',
+      'Anders ausgedrückt: Resilienz ist die Fähigkeit, für belastende Situationen immer den richtigen Schirm in der Tasche zu haben.',
+      'Was bedeutet dies nun für unser tägliches Leben?',
+      'Wir stehen fest am Boden und fallen auch dann nicht um, wenn wir für eine gewisse Zeit nur auf einem Bein stehen, denn mit dem Regenschirm können wir das Gleichgewicht halten.',
+      'Wir fühlen uns sogar so resilient, dass wir die Kraft haben, auch anderen Schutz zu bieten.',
+      'Wir wissen, dass wir jederzeit einen Schirm benutzen können, und das gibt uns ein Gefühl von Sicherheit.',
+      'Wir können uns „maßgeschneidert“ schützen, denn Schirme gibt es in vielen Größen und Farben.',
+      'Wir können uns mithilfe des passenden Schirms jederzeit den Gegebenheiten anpassen.',
+      'Wir können uns dort schützen, wo wir es gerade benötigen.',
+      'Diese Themen wollen wir im Resilienz-Training genauer betrachten:',
+      '1. Stress – Was ist Stress? Wie wirkt sich Stress emotional und körperlich aus?',
+      '2. Ressourcen und Ziele – Welche Ressourcen habe ich bereits und welche sollte ich ausbauen? Welche Ziele im Leben habe ich?',
+      '3. Bedürfnisse und Emotionen – Was sind unsere Grundbedürfnisse? Welchen Sinn haben unsere Gefühle?',
+      '4. Grenzen und Freiraum – Habe ich bereits einen persönlichen Freiraum entwickelt? Habe ich gesunde Grenzen identifiziert und gesetzt?',
+      '5. Konflikte und Kommunikation – Was sind Konflikte? Wie funktioniert effektive Kommunikation?',
+      '6. Vermeidung und erlernte Hilflosigkeit – Warum vermeiden wir? Was ist erlernte Hilflosigkeit?',
+      '7. Annehmen und Loslassen – Was bedeutet es, Situationen anzunehmen und loszulassen?',
+      '8. Selbstwirksamkeit und Selbstverantwortung – Wie kann ich selbstwirksam und selbstverantwortlich denken und handeln?',
+      '9. Entspannung und Schlaf – Habe ich für mich wirksame Methoden der Entspannung gefunden?',
+      '10. Glaubenssysteme und Überzeugungen – Wie entstehen innere Werte und Überzeugungsmuster?',
+      'Philosophie',
+      'Meine Herangehensweise ist lösungsorientiert. Ich arbeite nach dem Ansatz von Steve de Shazer. In Gesprächen erarbeiten wir gemeinsam, welche Wirkung bestimmte Gedanken und Verhaltensweisen haben. Dabei wird gleichzeitig klar, welche Gedanken und Verhaltensweisen Ihnen dabei helfen, Ihre Ziele zu erreichen.',
+      'Es werden immer die Stärken und positiven Verhaltensmuster des Einzelnen zur weiteren Lösungskonstruktion genutzt, die neue Türen im Erkennen, Fühlen und Handeln eröffnen. Während des Trainings unterstütze ich Sie dabei, neue Perspektiven zu entwickeln, Ihre bereits vorhandenen Ressourcen zu erkennen und hilfreiche Gedanken und Verhaltensweisen zu trainieren.',
+      'Ablauf des Trainings',
+      '1. Ihre Anmeldung: Sie kontaktieren mich per Mail oder telefonisch und wir vereinbaren einen Termin für das Erstgespräch.',
+      '2. Ihr Erstgespräch: Ein kurzes, kostenfreies Erstgespräch erlaubt uns ein gegenseitiges Kennenlernen und ermöglicht mir, ein Gespür für Ihr Anliegen zu bekommen. Damit ein Training erfolgreich ist, ist es entscheidend, dass Sie sich gut aufgehoben fühlen. So können Sie über eine weitere Zusammenarbeit entscheiden und wir haben die Möglichkeit, konkrete Vereinbarungen zu treffen.',
+      '3. Ihr Folgetermin / Ihre Folgetermine (45 Minuten – 50 €): So unterschiedlich wie die Anliegen meiner Klientinnen und Klienten sind, so unterschiedlich ist auch die Anzahl unserer gemeinsamen Gespräche. Manchmal reicht bereits eine Sitzung, in anderen Fällen sind mehrere Treffen sinnvoll.',
+      'Sie können dabei von Sitzung zu Sitzung entscheiden, ob Sie noch einmal kommen möchten. Lebenskrisen entwickeln sich meist über einen längeren Zeitraum und sind daher nicht auf „Knopfdruck“ zu lösen. Geduld ist der Schlüssel zum Erfolg.'
+    ]
+  };
+
+  private readonly contactDe = {
+    titel: 'KONTAKT',
+    paragraphs: [
+      'Für Terminabsprachen oder Fragen kontaktieren Sie mich gern.',
+      'Ich freue mich auf Ihre Nachricht.',
+      'Katja Wersuhn',
+      'Kramstr. 9',
+      '29410 Salzwedel',
+      'Telefon: 0174 / 384 67 42',
+      'Sollte ich persönlich gerade nicht erreichbar sein, hinterlassen Sie einfach eine Nachricht und dann melde ich mich bei Ihnen.',
+      'E-Mail: jetzt@glück-selbermachen.de'
+    ]
+  };
+
   get bewegung() {
     return this.toggleValue === 'false' ? this.bewegungDe : this.bewegungEn;
   }
 
   get home() {
     return this.toggleValue === 'false' ? this.homeDe : this.homeEn;
+  }
+
+  get aboutme() {
+    return this.toggleValue === 'false' ? this.aboutMeDe : this.aboutMeDe;
+  }
+
+  get resilience() {
+    return this.toggleValue === 'false' ? this.resilienceDe : this.resilienceDe;
+  }
+
+  get contact() {
+    return this.toggleValue === 'false' ? this.contactDe : this.contactDe;
   }
 
   toggleLanguage() {

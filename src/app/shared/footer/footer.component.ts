c25531f811abefef1b32b30d1e0ae8e5811be916
devcontain <devcontain@users.noreply.github.com>
2026-03-24 22:36:16 +0100

    footer and background-color

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { RouterLink, Router, NavigationEnd } from '@angular/router';
+import { LanguageService } from '../language.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  constructor(
+    public languageService: LanguageService,
+    private router: Router
+  ) {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    });
+  }
+
+  /**
+ * Scroll to bottom of page
+ */
+  back(): void {
+    const isHome = this.router.url === '/';
+
+    if (isHome) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      return;
+    }
+
+    this.router.navigate(['/'], { replaceUrl: true }).then(() => {
+      setTimeout(() => {
+        document.getElementById('footer')?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }, 0);
+    });
+  }
+
 }
+
+
